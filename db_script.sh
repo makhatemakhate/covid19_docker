e910dda65e4ee663620b7f83cbc9bf0fb2f6a790
Makhate Makhate <makhatemakhate@gmail.com>
2020-04-26 21:16:12 +0200

    making some few changes

--- db_script.sh
+++ db_script.sh
@@ -4,7 +4,7 @@ set -e
 sudo su postgres
 
 #create the covid19 database
-createdb -O covid19 postgres
+createdb -O postgres covid19
 
 #hopefully importing the database when creating the image ;)
-psql --user=postgres --dbname=covid19 -f /tmp/covid19_24042020.sql
+psql --user=postgres --dbname=covid19 < /tmp/covid19_24042020.sql
