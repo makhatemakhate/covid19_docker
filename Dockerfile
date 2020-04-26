ca7f905b9993ebe46004ae712bd8d761bd78fb30
Makhate Makhate <makhatemakhate@gmail.com>
2020-04-26 20:47:48 +0200

    stripping off unnecessary lines

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM omrsregrepo/bahmni_base:69_260718
-# bahmni updates happen here
+FROM mdillon/postgis:10-alpine
+# adding the scripts to import the database 
 ARG container_name
 ARG bind_mount_dest
 ADD db_script.sh /tmp/db_script.sh
 RUN chmod a+x /tmp/db_script.sh
-ADD development.sh /tmp/development.sh
-RUN chmod a+x /tmp/db_script.sh
-ADD backup.sql /tmp/backup.sql
-ENTRYPOINT /tmp/db_script.sh ; bahmni -ilocal start ; sh /tmp/db_script.sh ; /bin/bash
+ADD covid19_24042020.sql /tmp/covid19_24042020.sql
+ENTRYPOINT sh /tmp/db_script.sh ; /bin/bash
--- db_script.sh
+++ db_script.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
-container_name=$container_name
-if [ -f /etc/my.cnf ]; then
-    sudo mkdir -p /${container_name}/mysql && sudo chown -R mysql:mysql /${container_name}/mysql
-    sudo sed -i "s|datadir=/var/lib/mysql|datadir=/${container_name}/mysql|g" /etc/my.cnf
-    sudo service mysqld restart
-else
-   echo "File /etc/my.cnf does not exist."
-fi
+#switch user to postgres
+sudo su postgres
 
-if [ -n "$(ls -A /${container_name}/mysql)" ]; then
-    echo "Directory exists and Already mysql sync completed"
-else
-    sudo rsync -avr -o -g /var/lib/mysql /${container_name}
-    sudo service mysqld restart
-fi
+#create the covid19 database
+createdb -O covid19 postgres
+
+#hopefully importing the database when creating the image ;)
+psql --user=postgres --dbname=covid19 -f /tmp/covid19_24042020.sql
